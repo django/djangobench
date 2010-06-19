193c25d0a03b9d72efbfd0c6b32776060f61220a
Andrew Mosson <amosson@work-laptop.local>
2010-06-19 16:47:58 -0700

    Changed whitespace

--- utils.py
+++ utils.py
@@ -5,8 +5,8 @@ def run_benchmark(benchmark, syncdb=True, trials=1):
     if syncdb:
         from django.core.management import call_command
         call_command("syncdb")
-    cum_diff = 0.0
     
+    cum_diff = 0.0
     for x in xrange(trials):
         start = time()
         benchmark()
