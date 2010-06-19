04044161c63e48c7858f037d09a206346fc3a748
Alex Gaynor <alex.gaynor@gmail.com>
2010-06-19 14:49:16 -0500

    Added a template compilation benchmark

---
+++ benchmarks/template_compilation/benchmark.py
@@ -0,0 +1,28 @@
+from django.template import Template
+
+from utils import run_benchmark
+
+
+def benchmark():
+    # Just compile the template, no rendering
+    t = Template("""
+        {% for v in vals %}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+            {{ v }}
+        {% endfor %}
+    """)
+
+run_benchmark(benchmark, syncdb=False)
