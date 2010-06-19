876a49ea57be69eea4d5535a4b555e73ccd87662
Andrew Mosson <amosson@work-laptop.local>
2010-06-19 16:12:24 -0700

    Adding MultiValueDict Benchmark

---
+++ benchmarks/multi_value_dict/benchmark.py
@@ -0,0 +1,66 @@
+from time import time, sleep
+
+from django.utils.datastructures import MultiValueDict
+import django.utils.copycompat as copy
+
+
+
+case = {'a': ['a'], 'b': ['a','b'], 'c':['a','b','c']}
+update = {'a': ['a'], 'b': ['a','b'], 'c':['a','b','c']}
+
+
+def benchmark_multi():
+    #instantiate a new MultiValueDict and call key method (i.e. that do something diff than dict)
+    caseDict = MultiValueDict(case)
+
+    caseDict['a']
+    caseDict['b']
+    caseDict['c']
+    
+    caseDict.update(update)
+    copy.copy(caseDict)
+    copy.deepcopy(caseDict)
+    
+    caseDict.items()
+    caseDict.lists()
+    for i in caseDict:
+        i
+
+    caseDict['a'] = 'A'
+    caseDict['b'] = 'B'
+    caseDict['c'] = 'C'
+
+def benchmark_dict():
+    #instantiate a new dict and call same methods as above - to be fair, get unlistify in this method where required
+    caseDict = dict(case)
+    
+    caseDict['a'][0]
+    caseDict['b'][1]
+    caseDict['c'][2]
+    
+    caseDict.items()
+    caseDict.values()
+    for i in caseDict:
+        i
+    
+    caseDict.update(update)
+    copy.copy(caseDict)
+    copy.deepcopy(caseDict)
+    
+    caseDict['a'] = ['A']
+    caseDict['b'] = ['B']
+    caseDict['c'] = ['C']
+
+        
+diff = 0.0
+for x in range(200):
+    multi_time_start = time()
+    benchmark_multi()
+    multi_time_diff = time() - multi_time_start
+    dict_time_start = time()
+    benchmark_dict()
+    dict_time_diff = time() - dict_time_start
+    diff = diff + multi_time_diff - dict_time_diff
+print diff  
+
+
---
+++ benchmarks/multi_value_dict/settings.py
@@ -0,0 +1,7 @@
+INSTALLED_APPS = ['multi_value_dict']
+DATABASES = {
+    'default': {
+        'ENGINE':'sqlite3',
+        'NAME': ':memory:'
+    }
+}
