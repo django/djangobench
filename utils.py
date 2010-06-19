ba068cb625275895663b45f90a792025bcfaf5e0
Andrew Mosson <amosson@work-laptop.local>
2010-06-19 16:46:55 -0700

    Added trials to run_benchmark

--- utils.py
+++ utils.py
@@ -1,10 +1,15 @@
 from time import time
 
 
-def run_benchmark(benchmark, syncdb=True):
+def run_benchmark(benchmark, syncdb=True, trials=1):
     if syncdb:
         from django.core.management import call_command
         call_command("syncdb")
-    start = time()
-    benchmark()
-    print time() - start
+    cum_diff = 0.0
+    
+    for x in xrange(trials):
+        start = time()
+        benchmark()
+        cum_diff += time() - start
+        
+    print cum_diff
