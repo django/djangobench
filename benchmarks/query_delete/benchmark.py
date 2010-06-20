76f5811f29ad10d5275fc564dc2daff2cf50f7a0
sandy <sandy@sandy-desktop.(none)>
2010-06-19 18:46:07 -0700

    Adding benchmarks for query types: delete, get_or_create

---
+++ benchmarks/query_delete/benchmark.py
@@ -0,0 +1,11 @@
+import time
+
+from utils import run_benchmark
+
+from query_delete.models import Book
+
+def benchmark():
+    for b in Book.objects.all():
+        b.delete()
+
+run_benchmark(benchmark)
---
+++ benchmarks/query_get_or_create/benchmark.py
@@ -0,0 +1,10 @@
+import time
+
+from utils import run_benchmark
+
+from query_get_or_create.models import Book
+
+def benchmark():
+    Book.objects.get_or_create(id=1)
+
+run_benchmark(benchmark)
