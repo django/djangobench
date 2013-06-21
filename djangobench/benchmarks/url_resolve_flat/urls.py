b6a06e0a3dba93fb90d34ef13b3670a20009e855
Ramiro Morales <ramiro@rmorales.net>
2013-06-21 20:20:11 -0300

    URL mapper import change missed in 5b65d378a0bcaea43994c82a1db514d6376b0c76.

--- djangobench/benchmarks/url_resolve_flat/urls.py
+++ djangobench/benchmarks/url_resolve_flat/urls.py
@@ -1,4 +1,7 @@
-from django.conf.urls.defaults import patterns, include, url
+try:
+    from django.conf.urls import patterns, include, url
+except ImportError:
+    from django.conf.urls.defaults import patterns, include, url
 
 def ok_view(request, *a, **kw):
     pass
