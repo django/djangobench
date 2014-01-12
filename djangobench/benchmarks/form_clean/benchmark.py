24c4bd690f3d4af1128f5745894364db23277d15
Anssi Kääriäinen <akaariai@gmail.com>
2014-01-12 19:03:17 +0200

    Avoided calling Form.__init__ before django.setup() has been called

--- djangobench/benchmarks/form_clean/benchmark.py
+++ djangobench/benchmarks/form_clean/benchmark.py
@@ -1,15 +1,28 @@
 from django import forms
 from djangobench.utils import run_benchmark
 
+
 class BookForm(forms.Form):
     title = forms.CharField(max_length=100)
 
-form = BookForm({'title': 'hi'})
+form = None
+
+
+def setup():
+    # Can't initialize a form during import as __init__ uses
+    # ugettext
+    global form
+    form = BookForm({'title': 'hi'})
+
+
+def benchmark():
+    form.full_clean()
 
 run_benchmark(
-    form.full_clean,
-    syncdb = False,
-    meta = {
-    'description': 'Speed of a Form.clean call.',
-    }
+    benchmark,
+    syncdb=False,
+    meta={
+        'description': 'Speed of a Form.clean call.',
+    },
+    setup=setup
 )
